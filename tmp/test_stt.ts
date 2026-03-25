5f0c0d356028818e0466a50f34d341df248de61a
Chanyeong Yun <edward767976@gmail.com>
2026-03-25 21:44:44 +0900

    Removed unused comment

--- tmp/test_stt.ts
+++ tmp/test_stt.ts
@@ -6,9 +6,6 @@ async function test() {
     console.error("No API key");
     return;
   }
-
-  // We need an audio file. I will just create a tiny dummy or text for STT. Wait, I can just use curl with a dummy mp3 if I had one.
-  // Actually, I just need to read the STT API spec, or I can just check the backend's console log.
 }
 
 test();
